244c9a6e0157e1c4ac15711aff02b1041b9cda48
Swapnanil Dutta <47251193+swapnanildutta@users.noreply.github.com>
2021-01-06 15:08:01 +0530

    Smiling 🙂

---
+++ Day-25-(06.01.2021)/SmilingFace.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+import java.applet.*;
+
+/*<applet code="SmilingFace.class width=300 height=250></applet>*/
+
+public class SmilingFace extends Applet {
+    public void paint(Graphics g) {
+        g.setColor(Color.yellow);
+        g.fillOval(getWidth() / 2 - 130, getHeight() / 2 - 100, 260, 200);
+        g.setColor(Color.black);
+        g.fillOval(getWidth() / 2 - 80, getHeight() / 2 - 40, 20, 20);
+        g.fillOval(getWidth() / 2 + 65, getHeight() / 2 - 40, 20, 20);
+        g.drawArc(getWidth() / 2 - 80, getHeight() / 2 + 20, 160, 40, 180, 180);
+    }
+}
